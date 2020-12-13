248a575dba351913235c8d91e55302d016374712
Emilia Lozinska <em.lozinska@gmail.com>
2020-12-13 14:13:28 +0100

    Nueral network: Activation functions

--- 3_Neural_networks/helper.py
+++ 3_Neural_networks/helper.py
@@ -1,9 +1,17 @@
-# source: https://github.com/udacity/deep-learning-v2-pytorch
-
 import matplotlib.pyplot as plt
 import numpy as np
 
+def plot_multiple(input_x, plots):
+    # source: https://github.com/udacity/deep-learning-v2-pytorch
+    fig, axes = plt.subplots(figsize=(10,4), ncols=len(plots))
+    for index, plot_name in enumerate(plots):
+        axes[index].plot(input_x, plots[plot_name])
+        axes[index].grid(True)
+        axes[index].set_title(plot_name, fontweight="bold")
+    plt.tight_layout()
 
+# Code below:
+# source: https://github.com/udacity/deep-learning-v2-pytorch
 def test_network(network, trainloader):
     # Grab some data
     dataiter = iter(trainloader)
@@ -22,31 +30,19 @@ def test_network(network, trainloader):
 
     return True
 
-
-def view_classify(img, ps, version="MNIST"):
+def view_classify(image, output, version="MNIST"):
     ''' Function for viewing an image and it's predicted classes.
     '''
-    ps = ps.data.numpy().squeeze()
+    output = output.data.numpy().squeeze()
 
     fig, (ax1, ax2) = plt.subplots(figsize=(6,9), ncols=2)
-    ax1.imshow(img.resize_(1, 28, 28).numpy().squeeze())
+    ax1.imshow(image.resize_(1, 28, 28).numpy().squeeze())
     ax1.axis('off')
-    ax2.barh(np.arange(10), ps)
+    ax2.barh(np.arange(10), output)
     ax2.set_aspect(0.1)
     ax2.set_yticks(np.arange(10))
     if version == "MNIST":
         ax2.set_yticklabels(np.arange(10))
-    elif version == "Fashion":
-        ax2.set_yticklabels(['T-shirt/top',
-                            'Trouser',
-                            'Pullover',
-                            'Dress',
-                            'Coat',
-                            'Sandal',
-                            'Shirt',
-                            'Sneaker',
-                            'Bag',
-                            'Ankle Boot'], size='small');
     ax2.set_title('Class Probability')
     ax2.set_xlim(0, 1.1)
 
